ad9147b9a874e921f8d9b5d1fc07e0ea7518a1ca
mat <matd.japan@gmail.com>
2014-11-03 15:26:38 +0100

    bla

---
+++ Space Invaders/Assets/Scripts/InvadersManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvadersManager : MonoBehaviour
+{
+	public GameObject invader1Prefab;
+	public GameObject invader2Prefab;
+	public GameObject invader3Prefab;
+	
+	private bool goingRight;
+	
+	private Transform[] invaders;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		invaders = new Transform[55];
+		
+		int index = 0;
+		GameObject invader = invader1Prefab;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 11; j++)
+			{
+				if (i == 0 || i == 1)
+				{
+					invader = invader1Prefab;
+				}
+				if (i == 2 || i == 3)
+				{
+					invader = invader2Prefab;
+				}
+				if (i == 4)
+				{
+					invader = invader3Prefab;
+				}
+				
+				invader = Instantiate(invader) as GameObject;
+				
+				invader.transform.position = new Vector3(j * 2.2f, i * 2.2f, 0);
+				
+				invader.transform.parent = transform;
+				
+				invaders[index] = invader.transform;
+				index = index + 1;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	
+	public void moveInvadersCloser ()
+	{
+		
+	}
+	
+	public void destroyInvader (Transform invader)
+	{
+		for (int i = 0; i < invaders.Length; i++)
+		{
+			if (invaders[i] == invader)
+			{
+				Destroy(invader.gameObject);
+				invaders[i] = null;
+			}
+		}
+	}
+}
---
+++ Space Invaders/Assets/Scripts/Ship.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour
+{
+	/// <summary>
+	/// The bullet prefab.
+	/// </summary>
+	public GameObject bulletPrefab;
+	
+	private GUIText livesLabel;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		livesLabel = GameObject.Find("LivesLabel").guiText;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Left movement
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(-16 * Time.deltaTime, 0, 0);
+		}
+		// Right movement
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(16 * Time.deltaTime, 0, 0);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+			bullet.transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+			bullet.rigidbody2D.velocity = new Vector2(0, 20);
+		}
+	}
+	
+
+		private void shoot()
+		{
+			Transform bullet = Instantiate (bulletPrefab) as Transform;
+			bullet.transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+			bullet.rigidbody2D.velocity = new Vector2 (0, 50);
+			Transform bullet2 = Instantiate (bulletPrefab) as Transform;
+			bullet2.transform.position = new Vector3 (transform.position.x-1, transform.position.y + 0.5f, transform.position.z);
+			bullet2.rigidbody2D.velocity = new Vector2 (-7, 50);
+			Transform bullet3 = Instantiate (bulletPrefab) as Transform;
+			bullet3.transform.position = new Vector3 (transform.position.x+1, transform.position.y + 0.5f, transform.position.z);
+			bullet3.rigidbody2D.velocity = new Vector2 (7, 50);
+			Transform bullet4 = Instantiate (bulletPrefab) as Transform;
+			bullet4.transform.position = new Vector3 (transform.position.x-2, transform.position.y , transform.position.z);
+			bullet4.rigidbody2D.velocity = new Vector2 (-15, 50);
+			Transform bullet5 = Instantiate (bulletPrefab) as Transform;
+			bullet5.transform.position = new Vector3 (transform.position.x+2, transform.position.y , transform.position.z);
+			bullet5.rigidbody2D.velocity = new Vector2 (15, 50);
+		}
+	
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.collider.CompareTag("Invader"))
+		{
+			Destroy(collision.collider.gameObject);
+			transform.position = new Vector3(0, transform.position.y, 0);
+		}
+	}
+}
